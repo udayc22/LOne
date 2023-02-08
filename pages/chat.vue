40446393187ad8c5b088f02fe18737728efe6a91
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-08 18:07:47 +0530

    darkmode in OnMounted

--- pages/chat.vue
+++ pages/chat.vue
@@ -5,6 +5,12 @@
 <script setup>
 import Chat from "@/components/Chat.vue";
 
+onMounted(() => {
+  // THIS SCRIPT REQUIRED FOR PREVENT For Dark Mode
+  // localStorage.getItem("dark-mode") === "dark"
+  //  && document.documentElement.classList.add("dark");
+});
+
 // components: {
 //   Chat;
 // }
