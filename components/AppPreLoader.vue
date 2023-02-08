c3d132d9ba869719be85bf904aedb472dbf42327
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-08 16:53:51 +0530

    MainSidebar Abstracted

---
+++ components/AppPreLoader.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- App Preloader -->
+  <div
+    class="app-preloader fixed z-50 grid h-full w-full place-content-center bg-slate-50 dark:bg-navy-900"
+  >
+    <div class="app-preloader-inner relative inline-block h-48 w-48"></div>
+  </div>
+</template>
