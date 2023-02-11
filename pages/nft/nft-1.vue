1094706718bb16bb85ea00c6e0c32f59f02d1f36
Uday Chavan Git <udaymchavan228@gmail.com>
2023-02-11 17:44:17 +0530

    creaated nft1 & nft2 in components

---
+++ pages/nft/nft-1.vue
@@ -0,0 +1,9 @@
+<template>
+  <nft-1 />
+</template>
+
+<script setup>
+import Nft1 from "~~/components/Nft/Nft-1.vue";
+</script>
+
+<style scoped></style>
---
+++ pages/nft/nft-2.vue
@@ -0,0 +1,9 @@
+<template>
+  <Nft-2 />
+</template>
+
+<script setup>
+import Nft2 from "~~/components/Nft/Nft-2.vue";
+</script>
+
+<style scoped></style>
